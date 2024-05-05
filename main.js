383b7693c03aa481748d8e2ac55716e548b23c89
Vincent Clipet <vincent.clipet.7@gmail.com>
2024-05-06 00:04:52 +0200

    feat: Added orbit controls Added mini-sphere color shift

--- main.js
+++ main.js
@@ -1,72 +1,95 @@
-import * as THREE from 'three';
-import { GreaterEqualCompare } from 'three';
-import WebGL from 'three/addons/capabilities/WebGL.js';
-import Stats from 'three/addons/libs/stats.module.js'
+import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
+import Stats from "three/addons/libs/stats.module.js";
+import ColorUtils from "./utils/ColorUtils";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
 
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const stats = new Stats();
-document.body.appendChild(stats.dom)
-
-
-
-const geometry = new THREE.SphereGeometry( 500, 64, 64 );
-const material = new THREE.PointsMaterial({ color: 0xff0000, size: 2 }); 
-geometry.rotateX(Math.PI / 2)
-geometry.rotateX(Math.PI / 10)
-const points = new THREE.Points(geometry, material)
-
-
-const geometry2 = new THREE.SphereGeometry( 600, 64, 64 );
-geometry2.rotateX(Math.PI / 10)
-const material2 = new THREE.PointsMaterial({
-	color: 0xffff00,
-	size: 2
-}); 
-const points2 = new THREE.Points(geometry2, material2)
-
-
-
-
-const group = new THREE.Group();
-group.add(points)
-group.add(points2)
-
-
-
-camera.position.z = 1000;
-scene.add(group);
-
-
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	// Animation code here
-	// points.rotateX(0.002)
-	// group.rotateY(0.002)
-	points.rotateY(0.002)
-	points2.rotateY(-0.002)
-	// group.rotateZ(0.001)
-
-	renderer.render( scene, camera );
-
-	stats.update()
+function getRandomInt(max, min = 0) {
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
-if ( WebGL.isWebGLAvailable() ) {
+
+// Setup Scene
+const scene = new THREE.Scene();
+
+// Setup Camera
+const camera = new THREE.PerspectiveCamera(
+	30,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000000,
+);
+camera.position.z = 2200;
+
+// Setup Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Setup controls
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const orbs = new THREE.Group();
+
+const geometry = new THREE.SphereGeometry(500, 32, 32);
+const points = geometry.getAttribute("position").array;
+
+var data = new Array(points.length / 3);
+
+const gradient = ColorUtils.generateGradient(0x7734eb, 0xebde34, 60, true);
+console.log(gradient);
+
+for (var i = 0; i < points.length; i = i + 3) {
+	const geometry = new THREE.SphereGeometry(6, 4, 4);
+	
+	// Set orb location
+	geometry.translate(points[i], points[i + 1], points[i + 2]);
+	
+	// Set color & current gradient step for animation
+	const randomInt = getRandomInt(gradient.length);
+	console.log(gradient[randomInt])
+	const material = new THREE.MeshBasicMaterial({ color: gradient[randomInt].rgb });
+
+	const orb = new THREE.Mesh(geometry, material);
+	orbs.add(orb);
+	data[i / 3] = {
+		orb,
+		// x: points[i],
+		// y: points[i + 1],
+		// z: points[i + 2],
+		gradientStep: randomInt,
+	};
+}
+
+scene.add(orbs);
+
+function animate() {
+	const frame = requestAnimationFrame(animate);
+	controls.update();
+
+	// Animation code here
+	data.forEach((i) => {
+		i.gradientStep += 1;
+		const newRGB = gradient[i.gradientStep % gradient.length].rgb;
+		i.orb.material.color.setHex(newRGB);
+	});
+
+	renderer.render(scene, camera);
+
+	stats.update();
+}
+
+if (WebGL.isWebGLAvailable()) {
 	// Initiate function or other initializations here
 	animate();
 } else {
 	const warning = WebGL.getWebGLErrorMessage();
-	document.getElementById('container').appendChild(warning);
+	document.getElementById("container").appendChild(warning);
 }
